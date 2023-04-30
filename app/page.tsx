23184acfe60195544596d9ca60696edb710f189f
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-30 08:20:06 +0100

    Edited some misplaced folders. Worked on land page

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
        <p
        className='m-2 p-2 mx-14
        font-meduim font-Söhne 
-       text-center text-lg leading-7 tracking-wider'
+       text-center text-lg leading-7 tracking-wider sm:leading-7 sm:tracking-wider'
        >
         Are you preparing for the JAMB Computer-Based Test? Look no further than Jambite! Our website provides free study materials and resources to help you succeed on exam day. From practice questions to online mock exams, we&apos;ve got everything you need to ace the JAMB CBT. Sign up today and start your journey towards academic success!
        </p>
